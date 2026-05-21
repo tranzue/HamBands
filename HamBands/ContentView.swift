92a1d6f1b442af5cd364c208a5454a1437e9ba1d
Taylor Transue <taylor.transue@protonmail.com>
2026-05-21 13:35:20 -0400

    Initial Commit

---
+++ HamBands/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  HamBands
+//
+//  Created by ttransue on 5/21/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ HamBands/HamBandsApp.swift
@@ -0,0 +1,17 @@
+//
+//  HamBandsApp.swift
+//  HamBands
+//
+//  Created by ttransue on 5/21/26.
+//
+
+import SwiftUI
+
+@main
+struct HamBandsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
